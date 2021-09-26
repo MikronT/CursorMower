99725e83c19f10dce025b5adaf496c091df80d20
MikronT <vlad.ivanov.if@gmail.com>
2021-09-26 16:07:18 +0300

    Add help message

--- main.cpp
+++ main.cpp
@@ -5,6 +5,31 @@ using namespace std;
 using namespace nsStringLib;
 
 
+int help() {
+    cout << "Cursor Mover v1.0 [MikronT]" << endl
+        << endl
+        << "Usage" << endl
+        << "  cursor {x} {y} \"text\"" << endl
+        << "  cursor {left&right margin} {top&bottom margin} {x} {y} \"text\"" << endl
+        << endl
+        << "Notes" << endl
+        << "  - Coords are starting from (0,0) at the top left corner" << endl
+        << "  - Margins are applied to all the coords" << endl
+        << "    For example: 1st(0) line with top margin 2 will result in 3rd(2) line" << endl
+        << endl
+        << "Returns" << endl
+        << "  0 if all is OK" << endl
+        << "  1 if there are not enough or too many args" << endl
+        << "  2 when illegal argument specified (text instead of number)" << endl
+        << "  3 if the text is out of screen buffer bounds" << endl
+        << "  4 if help is shown" << endl;
+    return 4;
+}
+
+
 int main(const int arg_count, char** arg_list) {
+    if (arg_count == 1 ||
+        arg_count == 2 && string(arg_list[1]) == "/help")
+        return help();
     return 0;
 }
