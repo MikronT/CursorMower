b971cb9fe8182bbb8883c1d16435b3e292ddd97a
MikronT <vlad.ivanov.if@gmail.com>
2021-09-26 16:29:16 +0300

    Add test script

---
+++ test.cmd
@@ -0,0 +1,36 @@
+@echo off
+chcp 65001>nul
+
+setlocal EnableExtensions EnableDelayedExpansion
+
+pushd "%~dp0"
+
+set module_cursor=Debug\CursorMower.exe
+cls
+
+
+
+(
+  echo.dims`120`40
+  echo.margins`2`1
+
+  for /l %%i in (0,1, 39) do echo.line`0`%%i`^|
+  for /l %%i in (0,1, 39) do echo.line`119`%%i`^|
+  for /l %%i in (0,1,119) do echo.line`%%i`0`-
+  for /l %%i in (0,1,119) do echo.line`%%i`39`-
+
+  for /l %%i in (3,1,116) do (
+    for /l %%j in (2,1,37) do (
+      echo.line`%%i`%%j`+
+    )
+  )
+
+  echo.finally`1`39
+)>"layout.conf"
+
+
+
+:loop
+%module_cursor% "layout.conf"
+set /p input="> "
+goto :loop
