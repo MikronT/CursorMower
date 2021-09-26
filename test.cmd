40daea2fdfb06780cdd1302d443ffeaa66f6ec0b
MikronT <vlad.ivanov.if@gmail.com>
2021-09-26 16:40:31 +0300

    Update test script

--- test.cmd
+++ test.cmd
@@ -28,6 +28,19 @@ cls
     )
   )
 
+  echo.line`39`17`                                        
+  echo.line`39`18`                                        
+  echo.line`39`19`                                        
+  echo.line`39`20`                                        
+  echo.line`39`21`                                        
+  echo.line`39`22`                                        
+  echo.line`39`23`                                        
+  echo.line`39`24`                                        
+
+  echo.line`51`18`CursorMower v2.0
+  echo.line`53`20`1  Debug
+  echo.line`53`21`2  Release
+
   echo.final`51`23
 )>"layout.conf"
 
@@ -35,4 +48,7 @@ cls
 
 %module_cursor% "layout.conf"
 set /p input="> "
+
+       if "!input!" == "1" ( set module_cursor=Debug\CursorMower.exe
+) else if "!input!" == "2"   set module_cursor=Release\CursorMower.exe
 goto :loop
