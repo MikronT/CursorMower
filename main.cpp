f4835aeb4ba4350046158ca6bf42037cb57407a4
MikronT <vlad.ivanov.if@gmail.com>
2021-09-26 16:10:56 +0300

    Add some checks

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include "string.hpp"
 
@@ -31,5 +32,20 @@ int main(const int arg_count, char** arg_list) {
     if (arg_count == 1 ||
         arg_count == 2 && string(arg_list[1]) == "/help")
         return help();
+
+    if (arg_count != 4 && arg_count != 6) {
+        cerr << "Wrong number of arguments: " << to_string(arg_count - 1) << endl;
+        return 1;
+    }
+
+    for (int i = 1; i < arg_count - 2; ++i) {
+        if (const auto temp_arg = string(arg_list[i]);
+            !ranges::all_of(temp_arg, isdigit)) {
+            cerr << "Illegal argument: " << temp_arg << endl;
+            return 2;
+        }
+    }
+
+
     return 0;
 }
