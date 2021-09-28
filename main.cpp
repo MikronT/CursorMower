830a61a7d4e820e47565d7b397eb9ccf5f301444
MikronT <vlad.ivanov.if@gmail.com>
2021-09-28 14:16:03 +0300

    Rewrite parameter parser

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include "commandLine.hpp"
 #include "string.hpp"
@@ -21,83 +23,137 @@ int main(const int arg_count, char** arg_list) {
         return help();
 
     if (arg_count != 2)
-        error(ERROR_ARGS_COUNT, to_string(arg_count - 1));
+        error(ERROR_ARGS_COUNT,
+              to_string(arg_count - 1));
 
 
     const string arg_file = arg_list[1];
-    COORD arg_dims = {0, 0},
-          arg_margins = {0, 0};
-    vector<Block> arg_blocks;
+    COORD param_dims = {0, 0};
+    short param_margin = 0;
+    vector<Block> param_actions;
 
 
     //Parse input from the file
     ifstream layout;
     layout.open(arg_file);
     if (!layout.is_open())
-        error(ERROR_FILE, arg_file);
+        error(ERROR_FILE,
+              arg_file);
 
-    {
-        string line;
-        int line_i = 0;
-        auto line_coord = COORD{0, 0};
+    string line_read;
+    bool cursor_moved = true;
+    auto cursor1 = COORD{1, 1},
+         cursor2 = COORD{1, 1};
+    int line_i = 0;
 
-        while (getline(layout, line)) {
-            line_i++;
-            vector<string> cell = string_split(line, '`', 3);
-            if (cell.empty())
-                continue;
+    while (getline(layout, line_read)) {
+        line_i++;
+        vector<string> cells = string_split(line_read, '=', 2);
+        if (cells.empty())
+            continue;
 
-            if (cell.at(0) == "dims" ||
-                cell.at(0) == "margins" ||
-                cell.at(0) == "goto") {
-                if (cell.size() < 3)
-                    error(ERROR_FORMAT, to_string(line_i) + ": " + line);
+        //Check syntax of 1-value parameters
+        if (contains(
+            vector<string>{
+                "screen_width", "screen_height", "screen_margin",
+                "cursor1_up", "cursor1_down", "cursor1_left", "cursor1_right",
+                "cursor2_up", "cursor2_down", "cursor2_left", "cursor2_right",
+                "text"
+            },
+            cells.at(0))) {
+            if (cells.size() != 2)
+                error(ERROR_FORMAT, to_string(line_i) + ": " + line_read);
+        }
 
-                line_coord = COORD{
-                    to_short(cell.at(1), line_i),
-                    to_short(cell.at(2), line_i)
-                };
+
+        //Parse parameters
+        if (cells.at(0) == "screen_width")
+            param_dims.X = to_short(cells.at(1), line_i);
+
+        else if (cells.at(0) == "screen_height")
+            param_dims.Y = to_short(cells.at(1), line_i);
+
+        else if (cells.at(0) == "screen_margin")
+            param_margin = to_short(cells.at(1), line_i);
+
+        else if (cells.at(0) == "cursor1") {
+            vector<string> values = string_split(cells.at(1), ' ', 2);
+
+            if (values.size() < 2)
+                error(ERROR_FORMAT, to_string(line_i) + ": " + line_read);
+
+            cursor_moved = true;
+            cursor1 = COORD{
+                to_short(values.at(0), line_i),
+                to_short(values.at(1), line_i)
+            };
+        } else if (cells.at(0) == "cursor2") {
+            vector<string> values = string_split(cells.at(1), ' ', 2);
+
+            if (values.size() < 2)
+                error(ERROR_FORMAT, to_string(line_i) + ": " + line_read);
+
+            cursor_moved = true;
+            cursor2 = COORD{
+                to_short(values.at(0), line_i),
+                to_short(values.at(1), line_i)
+            };
+        } else if (cells.at(0) == "cursor1_up") {
+            cursor_moved = true;
+            cursor1.Y--;
+        } else if (cells.at(0) == "cursor1_down" || cells.at(0) == "skip") {
+            cursor_moved = true;
+            cursor1.Y++;
+        } else if (cells.at(0) == "cursor1_left") {
+            cursor_moved = true;
+            cursor1.X--;
+        } else if (cells.at(0) == "cursor1_right") {
+            cursor_moved = true;
+            cursor1.X++;
+        } else if (cells.at(0) == "cursor2_up") {
+            cursor_moved = true;
+            cursor2.Y--;
+        } else if (cells.at(0) == "cursor2_down") {
+            cursor_moved = true;
+            cursor2.Y++;
+        } else if (cells.at(0) == "cursor2_left") {
+            cursor_moved = true;
+            cursor2.X--;
+        } else if (cells.at(0) == "cursor2_right") {
+            cursor_moved = true;
+            cursor2.X++;
+        } else if (cells.at(0) == "text") {
+            if (cursor_moved) {
+                cursor_moved = false;
+                param_actions.emplace_back(Block{cursor1, {}});
             }
 
-            if (cell.at(0) == "dims")
-                arg_dims = line_coord;
+            param_actions
+                    .at(param_actions.size() - 1)
+                    .lines
+                    .emplace_back(cells.at(1));
+        } else if (cells.at(0) == "clear") {
+            int lines, length;
 
-            else if (cell.at(0) == "margins")
-                arg_margins = line_coord;
+            if (cells.size() == 1) {
+                lines = abs(cursor2.Y - cursor1.Y);
+                length = abs(cursor2.X - cursor1.X);
+            } else if (cells.at(1) == "screen") {
+                lines = param_dims.Y;
+                length = param_dims.X;
+            } else
+                error(ERROR_FORMAT, to_string(line_i) + ": " + line_read);
 
-            else if (cell.at(0) == "goto")
-                arg_blocks.emplace_back(Block{line_coord, {}});
+            auto text = string(length, ' ');
 
-            else if (cell.at(0) == "clear") {
-                auto lines =
-                    cell.size() == 1 ?
-                        arg_dims.Y :
-                        cell.size() == 2 ?
-                            1 :
-                            stoi(cell.at(2)) - arg_blocks.at(arg_blocks.size() - 1).coord.Y;
-                auto length =
-                    cell.size() == 1 ?
-                        arg_dims.X :
-                        to_short(cell.at(1), line_i);
-                auto text = string(length, ' ');
+            if (cells.size() == 1)
+                param_actions.emplace_back(Block{COORD{1, 1}, {}});
 
-                if (cell.size() == 1)
-                    arg_blocks.emplace_back(Block{COORD{0, 0}, {}});
-
-                for (int i = 0; i < lines; i++)
-                    arg_blocks
-                        .at(arg_blocks.size() - 1)
+            for (int i = 0; i < lines; i++)
+                param_actions
+                        .at(param_actions.size() - 1)
                         .lines
                         .emplace_back(text);
-            } else if (cell.at(0) == "text") {
-                if (cell.size() < 2)
-                    error(ERROR_FORMAT, to_string(line_i) + ": " + line);
-
-                arg_blocks
-                    .at(arg_blocks.size() - 1)
-                    .lines
-                    .emplace_back(cell.at(1));
-            }
         }
     }
 
@@ -107,42 +163,42 @@ int main(const int arg_count, char** arg_list) {
     //Get/set screen dimensions
     auto cmd = CommandLine();
 
-    if (arg_dims.X == 0 || arg_dims.Y == 0) {
+    if (param_dims.X == 0 || param_dims.Y == 0) {
         auto [X, Y] = cmd.getScreenDims();
 
-        arg_dims.X = X - arg_margins.X * 2;
-        arg_dims.Y = Y - arg_margins.Y * 2;
+        param_dims.X = X - param_margin * 2 * 2;
+        param_dims.Y = Y - param_margin * 2;
     } else {
-        arg_dims.X = arg_dims.X + arg_margins.X * 2;
-        arg_dims.Y = arg_dims.Y + arg_margins.Y * 2;
+        param_dims.X = param_dims.X + param_margin * 2 * 2;
+        param_dims.Y = param_dims.Y + param_margin * 2;
 
-        cmd.setScreenDims(arg_dims);
+        cmd.setScreenDims(param_dims);
     }
 
 
     //Write text to screen
-    for (auto& [coord, lines] : arg_blocks) {
+    for (auto& [coord, lines] : param_actions) {
         coord = {
-            to_short(coord.X + arg_margins.X),
-            to_short(coord.Y + arg_margins.Y)
+            to_short(coord.X + 1 + param_margin * 2),
+            to_short(coord.Y + 1 + param_margin)
         };
 
         if (lines.empty())
             cmd.goTo(coord);
         else
-            for (auto& i : lines) {
-                if (coord.X < 0 || coord.X >= arg_dims.X ||
-                    coord.Y < 0 || coord.Y >= arg_dims.Y)
+            for (auto& text : lines) {
+                if (coord.X < 1 || coord.X > param_dims.X ||
+                    coord.Y < 1 || coord.Y > param_dims.Y)
                     error(ERROR_OUT_OF_BOUNDS,
                           to_string(coord.X) + ";" +
                           to_string(coord.Y) +
                           " with bounds of " +
-                          to_string(arg_dims.X) + ":" +
-                          to_string(arg_dims.Y));
+                          to_string(param_dims.X) + ":" +
+                          to_string(param_dims.Y));
 
                 cmd.goTo(coord);
 
-                cout << string_cut(i, arg_dims.X - coord.X);
+                cout << string_cut(text, param_dims.X - coord.X);
 
                 coord = {coord.X, to_short(coord.Y + 1)};
             }
--- sample.cmd
+++ sample.cmd
@@ -23,58 +23,7 @@ if not exist "%module_cursor%" (
 
 
 :loop
-cls
-(
-  echo.screen_width=120
-  echo.screen_height=40
-  echo.screen_margin=1
-
-  for %%i in (1 120) do (
-    for /l %%j in (2,1,39) do (
-      echo.cursor_to=%%i %%j
-      echo.text=│
-    )
-  )
-  for %%i in (1 40) do (
-    for /l %%j in (2,1,119) do (
-      echo.cursor_to=%%j %%i
-      echo.text=─
-    )
-  )
-  echo.cursor_to=1 1
-  echo.text=┌
-  echo.cursor_to=120 1
-  echo.text=┐
-  echo.cursor_to=1 40
-  echo.text=└
-  echo.cursor_to=120 40
-  echo.text=┘
-
-  for %%i in (4 117) do (
-    for /l %%j in (2,1,39) do (
-      echo.cursor_to=%%i %%j
-      echo.text=▪
-    )
-  )
-  for %%i in (3 38) do (
-    for /l %%j in (2,1,119) do (
-      echo.cursor_to=%%j %%i
-      echo.text=▪
-    )
-  )
-
-  echo.cursor_from=40 15
-  echo.cursor_to=60 27
-  echo.clear
-
-  echo.cursor_to=49 17
-  echo.text=CursorMower Alpha v4.0
-  echo.cursor_to=54 19
-  echo.text=1  Check debug build
-  echo.text=2  Check release build
-  echo.skip
-  echo.text=0  Exit
-)>"layout.conf"
+call :prepareMainLayout
 
 set counter=0
 for /f "delims=" %%i in ('type "layout.conf" 2^>nul') do set /a counter+=1
@@ -109,3 +58,24 @@ set /p input="> "
 ) else if "!input!" == "1" ( set module_cursor=Debug\CursorMower.exe
 ) else if "!input!" == "2"   set module_cursor=Release\CursorMower.exe
 goto :loop
+
+
+
+
+
+:prepareMainLayout
+(
+  echo.screen_width=120
+  echo.screen_height=40
+  echo.screen_margin=1
+
+  echo.clear=screen
+
+  for %%i in (1 120) do (
+    for /l %%j in (2,1,39) do (
+      echo.cursor_to=%%i %%j
+      echo.text=│
+    )
+  )
+)>"layout.conf"
+exit /b
